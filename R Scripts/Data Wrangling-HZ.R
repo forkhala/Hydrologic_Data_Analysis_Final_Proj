6af391e1b5008d96a0014a92494c66960bc5b2d4
Haoyu Zhang <haoyu.zhang059@duke.edu>
2019-10-31 09:44:39 -0400

    Oct 31 by Haoyu Zhang Site selection for HUC1026, 1027, 1021

--- R Scripts/Data Wrangling-HZ.R
+++ R Scripts/Data Wrangling-HZ.R
@@ -59,3 +59,97 @@ huc1021.site.no #4 sites
 #---- HUC1021 end ----
 
 
+##### HUC 1026 #####
+
+huc1026all.site.no1 <- seq(from = 10260001, to = 10260010, by = 1)
+huc1026all.site.no2 <- seq(from = 10260010, to = 10260015, by = 1)
+
+
+# Get all sites within 1021
+huc1026.sites1 <- whatNWISdata(huc = huc1026all.site.no1) %>%
+  select(site_no, station_nm, huc_cd, site_tp_cd, dec_lat_va, dec_long_va,
+         parm_cd, data_type_cd, stat_cd, srs_id, begin_date, end_date, count_nu)
+huc1026.sites2 <- whatNWISdata(huc = huc1026all.site.no2) %>%
+  select(site_no, station_nm, huc_cd, site_tp_cd, dec_lat_va, dec_long_va,
+         parm_cd, data_type_cd, stat_cd, srs_id, begin_date, end_date, count_nu)
+huc1026.sites <- rbind(huc1026.sites1, huc1026.sites2)
+
+# Filter for sites with total nitrogen "00600"
+# (Total nitrogen [nitrate + nitrite + ammonia + organic-N], water, unfiltered, mg/l)
+huc1026.sites.N <- huc1026.sites %>%
+  filter(parm_cd == "00600") %>%
+  rename_at(vars(-(1:6)), ~ paste(., sep = "_",'N'))
+
+# Sites with total phosphorus "00665"
+# (Phosphorus, water, unfiltered, mg/l as phosphorus)
+huc1026.sites.P <- huc1026.sites %>%
+  filter(parm_cd == "00665") %>%
+  rename_at(vars(-(1:6)), ~ paste(., sep = "_",'P'))
+
+# Sites with discharge "00060"
+# (Discharge, cubic feet per second) there is also discharge in metric unit, but few sites have it.
+huc1026.sites.D <- huc1026.sites %>%
+  filter(parm_cd == "00060")%>%
+  rename_at(vars(-(1:6)), ~ paste(., sep = "_",'D'))
+
+# Find out the sites that are common in D N P data
+huc1026.sites.DNP <- huc1026.sites.D %>%
+  inner_join(., huc1026.sites.N, 
+             by = c("site_no","station_nm","huc_cd","site_tp_cd","dec_lat_va","dec_long_va"))%>%
+  inner_join(., huc1026.sites.P,
+             by = c("site_no","station_nm","huc_cd","site_tp_cd","dec_lat_va","dec_long_va"))%>%
+  filter(count_nu_D > 100 & count_nu_N > 50 & count_nu_P > 50)
+View(huc1026.sites.DNP)
+
+# Record site nos. of selected ones
+huc1026.site.no <- unique(huc1026.sites.DNP$site_no)
+huc1026.site.no
+
+#---- 1026 end ----
+
+##### 1027 #####
+
+huc1027all.site.no1 <- seq(from = 10270101, to = 10270104, by = 1)
+huc1027all.site.no2 <- seq(from = 10270201, to = 10270207, by = 1)
+
+# Get all sites within 1021
+huc1027.sites1 <- whatNWISdata(huc = huc1027all.site.no1) %>%
+  select(site_no, station_nm, huc_cd, site_tp_cd, dec_lat_va, dec_long_va,
+         parm_cd, data_type_cd, stat_cd, srs_id, begin_date, end_date, count_nu)
+huc1027.sites2 <- whatNWISdata(huc = huc1027all.site.no2) %>%
+  select(site_no, station_nm, huc_cd, site_tp_cd, dec_lat_va, dec_long_va,
+         parm_cd, data_type_cd, stat_cd, srs_id, begin_date, end_date, count_nu)
+huc1027.sites <- rbind(huc1027.sites1, huc1027.sites2)
+
+# Filter for sites with total nitrogen "00600"
+# (Total nitrogen [nitrate + nitrite + ammonia + organic-N], water, unfiltered, mg/l)
+huc1027.sites.N <- huc1027.sites %>%
+  filter(parm_cd == "00600") %>%
+  rename_at(vars(-(1:6)), ~ paste(., sep = "_",'N'))
+
+# Sites with total phosphorus "00665"
+# (Phosphorus, water, unfiltered, mg/l as phosphorus)
+huc1027.sites.P <- huc1027.sites %>%
+  filter(parm_cd == "00665") %>%
+  rename_at(vars(-(1:6)), ~ paste(., sep = "_",'P'))
+
+# Sites with discharge "00060"
+# (Discharge, cubic feet per second) there is also discharge in metric unit, but few sites have it.
+huc1027.sites.D <- huc1027.sites %>%
+  filter(parm_cd == "00060")%>%
+  rename_at(vars(-(1:6)), ~ paste(., sep = "_",'D'))
+
+# Find out the sites that are common in D N P data
+huc1027.sites.DNP <- huc1027.sites.D %>%
+  inner_join(., huc1027.sites.N, 
+             by = c("site_no","station_nm","huc_cd","site_tp_cd","dec_lat_va","dec_long_va"))%>%
+  inner_join(., huc1027.sites.P,
+             by = c("site_no","station_nm","huc_cd","site_tp_cd","dec_lat_va","dec_long_va"))%>%
+  filter(count_nu_D > 100 & count_nu_N > 50 & count_nu_P > 50)
+View(huc1027.sites.DNP)
+
+# Record site nos. of selected ones
+huc1027.site.no <- unique(huc1027.sites.DNP$site_no)
+huc1027.site.no
+
+#---- 1027 end ----
