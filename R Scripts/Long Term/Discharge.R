4071919c4222cf0c1df89cdf430d00e5ec7a536d
Haoyu Zhang <haoyu.zhang059@duke.edu>
2019-11-15 00:55:25 -0500

    Nov15 Haoyu. Finished Recurrence Analysis, and SD over years; results visualizaed and saved as csv as well

--- R Scripts/Long Term/Discharge.R
+++ R Scripts/Long Term/Discharge.R
@@ -266,24 +266,24 @@ for (i in 1:22) {
   model.full.sum <- summary(model.full)
   model.latest.sum <- summary(model.latest)
   
-  # fig <- ggplot() +
-  #   geom_point(data = Recurrence.full, aes(x = RecurrenceInterval, y = PeakDischarge),
-  #              color = "lightskyblue4", size = 1, alpha = 0.7)+
-  #   stat_smooth(data = Recurrence.full, aes(x = RecurrenceInterval, y = PeakDischarge),
-  #               method = "lm" , formula = y ~ log(x), 
-  #               level = 0.95, alpha = 0.3, size = 0.75, 
-  #               color = "lightskyblue4", fill = "lightskyblue3") +
-  #   geom_point(data = Recurrence.latest, aes(x = RecurrenceInterval, y = PeakDischarge), 
-  #              color = "darkseagreen4", size = 1, alpha = 0.7)+
-  #   stat_smooth(data = Recurrence.latest, aes(x = RecurrenceInterval, y = PeakDischarge),
-  #               method = "lm" , formula = y ~ log(x), 
-  #               level = 0.95, alpha = 0.4, size = 0.75, 
-  #               color = "darkseagreen4", fill = "darkseagreen3")+
-  #   annotate("text", x = -Inf, y = Inf, hjust = -0.05, vjust = 1, size = 3,
-  #            label = paste(site.list$huc_8[i], sep = " "))+
-  #   labs(x = element_blank(), y = element_blank())
+  fig <- ggplot() +
+    geom_point(data = Recurrence.full, aes(x = RecurrenceInterval, y = PeakDischarge),
+               color = "lightskyblue4", size = 1, alpha = 0.7)+
+    stat_smooth(data = Recurrence.full, aes(x = RecurrenceInterval, y = PeakDischarge),
+                method = "lm" , formula = y ~ log(x),
+                level = 0.95, alpha = 0.3, size = 0.75,
+                color = "lightskyblue4", fill = "lightskyblue3") +
+    geom_point(data = Recurrence.latest, aes(x = RecurrenceInterval, y = PeakDischarge),
+               color = "darkseagreen4", size = 1, alpha = 0.7)+
+    stat_smooth(data = Recurrence.latest, aes(x = RecurrenceInterval, y = PeakDischarge),
+                method = "lm" , formula = y ~ log(x),
+                level = 0.95, alpha = 0.4, size = 0.75,
+                color = "darkseagreen4", fill = "darkseagreen3")+
+    annotate("text", x = -Inf, y = Inf, hjust = -0.05, vjust = 1, size = 3,
+             label = paste("Site", site.list$site_lab[i], site.list$huc_cd[i], sep = " "))+
+    labs(x = element_blank(), y = element_blank())
     
-  #assign(paste0("RecPlot", site.list$huc4[i]), fig)
+  assign(paste0("RecPlot", formatC(i, width = 2, flag = "0")), fig)
   assign(paste("mod", formatC(i, width = 2, flag = "0"), "full", sep = "."), model.full)
   assign(paste("mod", formatC(i, width = 2, flag = "0"), "latest", sep = "."), model.latest)
   assign(paste("sum", formatC(i, width = 2, flag = "0"), "full", sep = "."), model.full.sum)
@@ -313,20 +313,25 @@ for (i in 1:22) {
 }
 
 pred.peakdischarge <- pred.peakdischarge %>%
-  mutate(diff = latest - full)
+  mutate(diff = latest - full) %>%
+  rename(post1980 = latest)
 View(pred.peakdischarge)
-#write.csv(pred.peakdischarge, file = "./Data/Processed/recurrence_int.csv", row.names = F)
-pred.peakdischarge <- read.csv("./Data/Processed/recurrenceint.csv")
+#write.csv(pred.peakdischarge, file = "./Data/Processed/recurrence_int_50.csv", row.names = F)
+pred.peakdischarge <- read.csv("./Data/Processed/recurrence_int_50.csv") %>%
+  arrange(diff)
+
 #---- loop end ----
 
-##### Combine all discharge plots #####
+##### Combine all Recurrence plots #####
 
 # blue is full; green is latest
-RecPlot <- ggarrange(RecPlot1020, RecPlot1021, RecPlot1022, RecPlot1023, RecPlot1024, RecPlot1025, 
-                     RecPlot1026, RecPlot1027, RecPlot1028, RecPlot1029, 
-                     RecPlot1030 + font("x.text", size = 10), ncol = 5, nrow = 5)
+RecPlot <- ggarrange(RecPlot01, RecPlot02, RecPlot03, RecPlot04, RecPlot05, RecPlot06, 
+                     RecPlot07, RecPlot08, RecPlot09, RecPlot10, RecPlot11, RecPlot12,
+                     RecPlot13, RecPlot14, RecPlot15, RecPlot16, RecPlot17, RecPlot18,
+                     RecPlot19, RecPlot20, RecPlot21, RecPlot22
+                     + font("x.text", size = 10), ncol = 5, nrow = 5)
 annotated.RecPlot <- 
-  annotate_figure(RecPlot, bottom=text_grob("Recurrence",color = "black",hjust = 0.5,size = 13),
+  annotate_figure(RecPlot, bottom=text_grob("Recurrence Interval",color="black",hjust = 0.5,size = 13),
                   left = text_grob(expression("Discharge ft"^3*" s"^-1), color = "black",
                                    rot = 90, size = 13))
 
@@ -351,24 +356,28 @@ for (i in 1:22) {
   model <- lm(data = std, SD ~ Year)
   model.sum <- summary(model)
   
-  # fig <- ggplot() +
-  #   geom_point(data = site, aes(x = Year, y = SD),
-  #              color = "lightskyblue4", size = 1, alpha = 0.7)+
-  #   stat_smooth(data = site, aes(x = Year, y = SD),
-  #               method = "lm" , formula = y ~ x, 
-  #               level = 0.95, alpha = 0.3, size = 0.75, 
-  #               color = "lightskyblue4", fill = "lightskyblue3") +
-  #   annotate("text", x = -Inf, y = Inf, hjust = -0.05, vjust = 1, size = 3,
-  #            label = paste(site.list$huc_8[i], sep = " "))+
-  #   labs(x = element_blank(), y = element_blank())
+  fig <- ggplot() +
+    geom_point(data = std, aes(x = Year, y = SD),
+               color = "lightskyblue4", size = 1, alpha = 0.7)+
+    stat_smooth(data = std, aes(x = Year, y = SD),
+                method = "lm" , formula = y ~ x,
+                level = 0.95, alpha = 0.3, size = 0.75,
+                color = if_else(model$coefficients[[2]] > 0, "brown2", "mediumseagreen"), 
+                fill = if_else(model$coefficients[[2]] > 0, "brown2", "mediumseagreen")) +
+    annotate("text", x = -Inf, y = Inf, hjust = -0.05, vjust = 1, size = 3,
+             label = paste("Site", site.list$site_lab[i], site.list$huc_cd[i], sep = " "),
+             color = if_else(model.sum$coefficients[2,4] < 0.05, "tomato2", "black"))+
+    labs(x = element_blank(), y = element_blank())
   
-  #assign(paste0("RecPlot", site.list$huc4[i]), fig)
+  #color = "lightskyblue4", fill = "lightskyblue3"
+  
+  assign(paste0("SdPlot", formatC(i, width = 2, flag = "0")), fig)
   assign(paste("mod", formatC(i, width = 2, flag = "0"), "sd", sep = "."), model)
   assign(paste("sum", formatC(i, width = 2, flag = "0"), "sd", sep = "."), model.sum)
-  rm(site, std, model, model.sum)
+  rm(site, std, model, model.sum, fig)
 }
 
-##### Put results in data frame #####
+##### Put results in data frame
 models.sd <- mget(x = ls(pattern = "^mod.\\d\\d.sd$"))
 sums.sd <- mget(x = ls(pattern = "^sum.\\d\\d.sd$"))
 
@@ -383,7 +392,9 @@ sd.year <- site.list %>%
   mutate(slope = NA, p = NA)
 
 for (i in 1:22) {
-  sd.year$slope[[i]] <- coef(models.sd[[i]])[[2]]
+  sd.year$slope[[i]] <- coef(models.sd[[i]])[[2]] %>%
+    round(., digits = 3) %>%
+    format(nsmall = 3)
   sd.year$p[[i]] <- sums.sd[[i]]$coefficients[2,4] %>%
     round(., digits = 4) %>%
     format(nsmall = 4)
@@ -395,4 +406,20 @@ View(sd.year)
 sd.year <- read.csv("./Data/Processed/sd_year.csv")
 
 #---- loop end ----
+
+##### Combine all SD vs year plots #####
+
+SdPlot <- ggarrange(SdPlot01, SdPlot02, SdPlot03, SdPlot04, SdPlot05, SdPlot06, 
+                    SdPlot07, SdPlot08, SdPlot09, SdPlot10, SdPlot11, SdPlot12,
+                    SdPlot13, SdPlot14, SdPlot15, SdPlot16, SdPlot17, SdPlot18,
+                    SdPlot19, SdPlot20, SdPlot21, SdPlot22
+                    + font("x.text", size = 10), ncol = 5, nrow = 5)
+annotated.SdPlot <- 
+  annotate_figure(SdPlot, bottom=text_grob("Year",color = "black",hjust = 0.5,size = 13),
+                  left = text_grob(expression("Standard Deviation within a Year"), color = "black",
+                                   rot = 90, size = 13))
+
+ggsave("./Figures/sd_year.jpg", annotated.SdPlot,
+       dpi = 300, width = 14, height = 9, units = "in")
+#---- Plot end ----
 
