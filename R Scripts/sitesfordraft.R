42d1da4a1d1534a0db164bacc91adc959eebed84
Caroline Watson <caroline.watson@duke.edu>
2019-11-18 18:39:45 -0500

    Sites for draft script

---
+++ R Scripts/sitesfordraft.R
@@ -0,0 +1,7 @@
+bestsites.tc <- bestsites.wq.skinny %>%
+  filter(Site == "06775900" | Site == "06810000" | Site == "06856600" |
+           Site == "06902000" | Site == "06934500" | Site == "06905500")
+
+06810000
+06934500
+06856600
